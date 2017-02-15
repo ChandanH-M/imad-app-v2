71b6de6b9f10de54589edeb11b3b468abc583373
ChandanH-M <chANDANHM06@GMAIL.COM>
2017-02-15 23:04:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,24 @@
 var button = document.getElementById('counter');
-var count = 0;
+
 button.onclick = function () {
-  count = count + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = count;
-};
+    
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function () {
+    
+    if(request.Readystate === XMLHttpRequest.DONE) {
+        
+        if(request.status === 200) {
+            
+            var counter = request.responseText;
+            
+            var span = document.getElementById('count');
+            
+            span.innerHTML = counter;
+            
+                }
+            }
+        };
+    request.open('GET','http://chandanh-m.imad.hasura-app.io/counter',true);
+    request.send(null);
+    };
