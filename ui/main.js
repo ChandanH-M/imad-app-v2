25fe05feb8a43de0f283914ff73f6266a5a34c9d
ChandanH-M <chANDANHM06@GMAIL.COM>
2017-02-16 00:51:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,12 +26,12 @@ request.onreadystatechange = function () {
     var value = nameInput.value;
     var submit = document.getElementById('submit');
     submit.onclick = function () {
-      var names = ['name1','name2','name3'];
-      var li = '';
-      for (var i = 0; i < names.length; i++) {
-           li += '<li>' + names[i] +'</li>';
-      }
-    var ul = document.getElementById('ordered');
-    ul.innerHTML = li;
+                    var names = ['name1','name2','name3'];
+                    var list = '';
+                         for (var i = 0; i < names.length; i++) {
+                             li += '<li>' + names[i] +'</li>';
+                    }
+                var ul = document.getElementById('ordered');
+                ul.innerHTML = list;
     
-};
+                };
