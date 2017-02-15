3b0bb88de10183845620defc94033edd5dc554ce
ChandanH-M <chANDANHM06@GMAIL.COM>
2017-02-15 20:35:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,12 @@ function createTemplate(data)
     return template;
     
 }
+var counter = 0;
 
+app.get('/counter',function (req,res){
+   counter = counter + 1;
+   res.send(counter.toString());
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
