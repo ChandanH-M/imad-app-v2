c1ab9f476590d8d43521100ea94eb081982ad347
ChandanH-M <chANDANHM06@GMAIL.COM>
2017-02-16 01:41:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,15 @@ request.onreadystatechange = function () {
     
     submit.onclick = function ()
         {
-                    var names = ['name1','name2','name3'];
+            var request = new XMLHttpRequest();
+
+request.onreadystatechange = function () {
+    
+    if(request.readyState === XMLHttpRequest.DONE) {
+        
+        if(request.status === 200) {
+              var names = request.responseText;
+              names = JSON.parse(names);
                     var list = '';
                              for (var i = 0; i < names.length; i++)
                              {
@@ -38,5 +46,13 @@ request.onreadystatechange = function () {
                              }
                             var ul = document.getElementById('ordered');
                             ul.innerHTML = list;
-    
+            
+           
+            
+                }
+            }
         };
+    request.open('GET','http://chandanh-m.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
+    };
+                  
