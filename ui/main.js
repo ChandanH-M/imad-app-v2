4dbe620270d3a9903e623ef441b8c1bab7dfa9f4
ChandanH-M <chANDANHM06@GMAIL.COM>
2017-02-15 19:01:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var element = document.getElementById('text');
+element.innerHTML = "new value";
+var img = document.getElementById('madi');
+img.onclick = function()
+{
+    img.style.marginRight = '100px';
+};
