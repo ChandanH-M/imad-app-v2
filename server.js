897f841e4e5f5b1b1fd7f938da624db34f5ce31a
ChandanH-M <chANDANHM06@GMAIL.COM>
2017-02-16 20:08:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,9 +133,9 @@ app.get('/counter',function (req,res){
    counter = counter + 1;
    res.send(counter.toString());
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+//app.get('/', function (req, res) {
+  //res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+//});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
